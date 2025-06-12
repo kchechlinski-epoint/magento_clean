12e98ef9becec5fc98e1e8ed2537861d010221f3
kchechlinski-epoint <72787629+kchechlinski-epoint@users.noreply.github.com>
2025-06-12 15:11:57 +0200

    Remove nonexistent service dependency from CompanyList collection

--- app/code/Epoint/CompanyList/Model/ResourceModel/Company/Collection.php
+++ app/code/Epoint/CompanyList/Model/ResourceModel/Company/Collection.php
@@ -11,18 +11,15 @@ use Magento\Framework\Model\ResourceModel\Db\Collection\AbstractCollection;
 
 class Collection extends AbstractCollection
 {
-    protected $nonExistentService;
 
     public function __construct(
         \Magento\Framework\Data\Collection\EntityFactoryInterface $entityFactory,
         \Psr\Log\LoggerInterface $logger,
         \Magento\Framework\Data\Collection\Db\FetchStrategyInterface $fetchStrategy,
         \Magento\Framework\Event\ManagerInterface $eventManager,
-        \Epoint\CompanyList\Service\NonExistentService $nonExistentService,
         \Magento\Framework\DB\Adapter\AdapterInterface $connection = null,
         \Magento\Framework\Model\ResourceModel\Db\AbstractDb $resource = null
     ) {
-        $this->nonExistentService = $nonExistentService;
         parent::__construct($entityFactory, $logger, $fetchStrategy, $eventManager, $connection, $resource);
     }
 
